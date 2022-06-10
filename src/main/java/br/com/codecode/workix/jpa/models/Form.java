2a39de984bf9d68a8c403b639557faa300ca07b4
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-10 09:24:40 -0300

    added form model

---
+++ src/main/java/br/com/codecode/workix/jpa/models/Form.java
@@ -0,0 +1,81 @@
+package br.com.codecode.workix.jpa.models;
+
+import br.com.codecode.workix.cdi.qualifiers.Persist;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "forms")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.PUBLIC_MEMBER)
+@Persist
+public class Form extends MyEntity{
+    private static final long serialVersionUID = 4441917823240375513L;
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String subject;
+
+    private String message;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NotEmpty
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NotEmpty
+    @Column
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @NotEmpty
+    @Column
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @NotEmpty
+    @Column
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

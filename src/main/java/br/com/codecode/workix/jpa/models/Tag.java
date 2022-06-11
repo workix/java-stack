a42e98c937ae27a8f8b126638a111fd7566eb405
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-11 11:53:06 -0300

    fixed tag duplication bug

--- src/main/java/br/com/codecode/workix/jpa/models/Tag.java
+++ src/main/java/br/com/codecode/workix/jpa/models/Tag.java
@@ -36,7 +36,22 @@ public class Tag implements Serializable{
     /**
      * Public Default Constructor for JPA Compatibility Only
      */
-    public Tag(){}    
+    public Tag(){}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        return name.equals(tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 
     /**
      * Public Constructor for {@link Builder} Compatibility

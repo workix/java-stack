f9be64c818aaeae03e8bfb323b701563c02f2792
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-06-15 09:11:00 -0300

    removing unused

--- src/main/java/br/com/codecode/workix/config/MailConfiguration.java
+++ src/main/java/br/com/codecode/workix/config/MailConfiguration.java
@@ -19,8 +19,8 @@ import javax.mail.MailSessionDefinition;
  * @since 1.0
  * @version 1.1
  */
-@MailSessionDefinition(name = "java:jboss/mail/fake", host = "smtp.gmail.com", user = "fake@gmail.com",
-    password = "fake@fake", transportProtocol = "smtp", from = "fake@gmail.com", properties = { "mail.smtp.port=465" })
+/*@MailSessionDefinition(name = "java:jboss/mail/fake", host = "smtp.gmail.com", user = "fake@gmail.com",
+    password = "fake@fake", transportProtocol = "smtp", from = "fake@gmail.com", properties = { "mail.smtp.port=465" })*/
 @Startup
 @Singleton
 public class MailConfiguration{}
